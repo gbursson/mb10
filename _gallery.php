8c36bdc1e0c243ef367b6dd9cd117778d6133b46
Gbursson <gbursson@gmail.com>
2017-12-11 14:55:17 +0000

    adding gallery

--- _gallery.php
+++ _gallery.php
@@ -4,24 +4,23 @@ $imagesPath = "img/gallery/";
 $imagesPerRow = 3;
 ?>
 
-<div class="block-group" style="background: url('img/art-title-bg.jpg') repeat; color: #FFFFFF; margin-bottom: 5%; padding: 20px;">
+<div class="b-g gallery-header" style="">
     <div class="block">
-        <div class="block-group" style="margin: 10px 20% 0 20%;">
-            <div class="block f-l w-80" style="font-size: 2em; font-weight: 600;">GALERIA ZDJĘĆ</div>
+        <div class="b-g" style="margin: 10px 20% 0 20%;">
+            <div class="block f-l w-80" style="font-size: 1.2em; font-weight: 600;">GALERIA ZDJĘĆ</div>
             <div class="block f-r w-20">
-                <div class="block-group" style="text-align: center;">
-                    <div class="block w-45" style="margin-left: -30px;"><img style="height: 4em;" src="img/photo.svg"></div>
+                <div class="b-g" style="text-align: right;">
+                    <div class="block"><img style="height: 2em;" src="img/photo.svg"></div>
 
                 </div>
             </div>
         </div>
     </div>
-
 </div>
 
 <?php
 
-echo('<div style="text-align justify; margin: 10px 20%;">');
+echo('<div style="text-align: center; margin: 10px 20%;">');
 
 for($i=1;$i<=$imagesCount;$i++) {
 
