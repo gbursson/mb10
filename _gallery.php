efc88d3eba968f6f60329539c10f603dd1a60254
Gbursson <gbursson@gmail.com>
2017-12-11 14:33:27 +0000

    adding gallery

--- _gallery.php
+++ _gallery.php
@@ -2,8 +2,26 @@
 $imagesCount = 6;
 $imagesPath = "img/gallery/";
 $imagesPerRow = 3;
+?>
 
-echo('<div style="text-align: center; margin: %2 10%;">');
+<div class="block-group" style="background: url('img/art-title-bg.jpg') repeat; color: #FFFFFF; margin-bottom: 5%; padding: 20px;">
+    <div class="block">
+        <div class="block-group" style="margin: 10px 20% 0 20%;">
+            <div class="block f-l w-80" style="font-size: 2em; font-weight: 600;">GALERIA ZDJĘĆ</div>
+            <div class="block f-r w-20">
+                <div class="block-group" style="text-align: center;">
+                    <div class="block w-45" style="margin-left: -30px;"><img style="height: 4em;" src="img/photo.svg"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php
+
+echo('<div style="text-align justify; margin: 10px 20%;">');
 
 for($i=1;$i<=$imagesCount;$i++) {
 
---
+++ mb10-form.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$email_szkolki = "dsplipno@wp.pl";
+//$email_szkolki = "gbursson@gmail.com";
+//$email_szkolki = "annasikorska@poczta.onet.pl";
+
+$name = $_POST['name'];
+$childname = $_POST['childname'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+$agegroup = $_POST['agegroup'];
+
+$subject = "[Szkółka] - wiadomość od: " . htmlspecialchars($name);
+$msg .= "<h3>Treść wiadomości</h3>";
+$msg .= htmlspecialchars($message) . "<br><br>";
+
+$msg .= "<b>Imię dziecka:</b> " . htmlspecialchars($childname) . "<br>";
+$msg .= "<b>Telefon:</b> " . htmlspecialchars($phone) . "<br>";
+$msg .= "<b>Email:</b> " . htmlspecialchars($email) . "<br>";
+$msg .= "<b>Grupa wiekowa:</b> " . $agegroup . "<br>";
+
+//DEBUG ONLY
+//echo $subject;
+//echo $msg;
+
+
+$headers = 'From:' . $email . "\r\n";
+$headers .= 'Cc:' . $email . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+$msg = wordwrap($msg, 70);
+
+mail($email_szkolki, $subject, $msg, $headers);
+
+include("_thanks.html");
+?>
